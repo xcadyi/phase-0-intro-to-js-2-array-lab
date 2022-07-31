2c8169b5e4e0debf2b4bd97a33336ca862a95dbd
Selin Ayyildiz <xcadyix@gmail.com>
2022-07-31 19:33:50 -0400

    commit tree 2bf0376b1bc04c1a76fa0cdfdc8bc0c72a306728

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+} 
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+cats.shift()
+}
+
+function appendCat() {
+const newCats = [...cats,"Broom"]
+return newCats
+}
+
+function prependCat() {
+const newCats = ["Arnold", ...cats]
+return newCats
+}
+
+function removeLastCat() {
+const newCats = cats.slice(0,cats.length - 1)
+return newCats
+}
+
+function removeFirstCat() {
+const newCats = cats.slice(1)
+return newCats
+}
